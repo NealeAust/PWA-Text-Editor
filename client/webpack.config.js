05a14e19f412149da927a95255c87e2013947a06
NealeAust <nealechapman@optushome.com.au>
2023-04-12 12:00:09 +1000

    Added script to package.json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,6 +6,28 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
+module: {
+  rules: [
+    //CSS Loaders
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.m?js$/,
+      exclude: /node_modules/,
+      
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+                       
+        },
+      },
+    },
+  ],
+},
+
 module.exports = () => {
   return {
     mode: 'development',
